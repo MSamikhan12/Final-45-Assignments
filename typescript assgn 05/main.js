27067dcb15991953eee77feeb3598d51344a21a1
muhammad sami <muhammadsamikhan2266@gmail.com>
2024-03-12 22:41:37 +0500

    first commit

---
+++ typescript assgn 05/main.js
@@ -0,0 +1,5 @@
+//storing the famous person’s name in a variable
+var famousPerson = "William Shakespeare";
+var message = "The legendary ".concat(famousPerson, " once said, \"I would not wish any companion in the world but you.\"");
+//printing the message
+console.log(message);
---
+++ typescript assgn 05/main.ts
@@ -0,0 +1,8 @@
+//storing the famous person’s name in a variable
+
+let famousPerson: string = "William Shakespeare";
+let message: string = `The legendary ${famousPerson} once said, "I would not wish any companion in the world but you."`;
+
+//printing the message
+
+console.log(message);
